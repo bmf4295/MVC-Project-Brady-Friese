e08faa44b21568736fa77424e618b62272cd2cda
Brady Friese <bmf4295@g.rit.edu>
2020-10-19 19:46:28 -0400

    Fixed Csurf error and finished Domomaker C

--- server/app.js
+++ server/app.js
@@ -72,6 +72,7 @@ app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(csrf());
 app.use((err,req,res,next)=>{
+  console.log(err.code)
   if(err.code !== 'EBADCSRFTOKEN') return next(err);
   
   console.log('Missing CSRF token');
